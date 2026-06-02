268725100a00210182bb44f28be5419770b5da17
yanghaohsiang <sseepers7777@gmail.com>
2026-06-02 23:35:28 +0800

    Deploy furniture catalog PWA

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FURNITURE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBflzaANT9BnCbMfBVbjvxyM8KnirJBAvo",
+  authDomain: "i-expense-pwa.firebaseapp.com",
+  projectId: "i-expense-pwa",
+  appId: "1:160747680735:web:1ce0be9463eb033f318a6a",
+  messagingSenderId: "160747680735",
+  storageBucket: "i-expense-pwa.firebasestorage.app"
+};
